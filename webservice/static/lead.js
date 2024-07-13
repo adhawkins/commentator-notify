9c4ca19d5b6952de6d606e0d8b820dd172bc5090
Andy Hawkins <andy@gently.org.uk>
2024-07-13 03:14:16 +0100

    Add voice selection

--- webservice/static/lead.js
+++ webservice/static/lead.js
@@ -18,9 +18,72 @@ function connectWS() {
 	}
 }
 
+let voices = [];
+
 function say(word) {
 	const speak = new SpeechSynthesisUtterance(word);
+
+	let voiceControl = document.getElementById('voice');
+	let selectedVoice = voiceControl.options[voiceControl.selectedIndex].text;
+
+	if (selectedVoice && voices.length) {
+		const voice = voices.find((voice) => voice.name == selectedVoice);
+		if (voice) {
+			speak.voice = voice;
+		}
+	}
+
 	window.speechSynthesis.speak(speak);
+}
+
+function voiceChanged() {
+	let voiceControl = document.getElementById('voice');
+	let selectedVoice = voiceControl.options[voiceControl.selectedIndex].text;
+
+	const url = new URL(window.location);
+	url.searchParams.set('voice', selectedVoice);
+
+	let urlControl = document.getElementById('url');
+	const newA = document.createElement('a');
+	newA.setAttribute('href', url.href);
+	newA.innerHTML = url.href;
+	urlControl.innerHTML = "";
+	urlControl.appendChild(newA);
+
+}
+
+function loadVoices() {
+	voices = window.speechSynthesis.getVoices();
+
+	if (voices.length) {
+		let voiceControl = document.getElementById('voice');
+
+		while (voiceControl.options.length > 0) {
+			voiceControl.remove(0);
+		}
+
+		const url = new URL(window.location);
+		const selectedVoice = url.searchParams.get('voice');
+		voices.forEach((voice) => {
+			if (voice.lang == "en-GB") {
+				const newOption = new Option(voice.name)
+
+				if (selectedVoice && selectedVoice == voice.name) {
+					newOption.selected = true;
+				}
+
+				voiceControl.add(newOption);
+			}
+		});
+
+		voiceChanged();
+	}
+}
+
+if ("onvoiceschanged" in window.speechSynthesis) {
+	window.speechSynthesis.onvoiceschanged = loadVoices;
+} else {
+	loadVoices();
 }
 
 connectWS();
